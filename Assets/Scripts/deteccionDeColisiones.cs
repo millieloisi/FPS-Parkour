ef1e018d356ffe77844a46ba125eabb58837eb97
millieloisi <167239633+millieloisi@users.noreply.github.com>
2025-05-28 15:50:48 -0300

    Eliminacion de Caja

---
+++ Assets/Scripts/deteccionDeColisiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto " + collision.gameObject.name);
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/mercancioScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mercancioScript : MonoBehaviour
+{
+
+    public int precio;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject);
+        if (collision.gameObject.name ==  "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+ 
